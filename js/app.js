8b8ca4a454ef8561bc114918ec6ce8ea6281b71e
shahinurrahman22004 <shahinur02345@gmail.como>
2021-09-02 12:41:03 +0600

    last

--- js/app.js
+++ js/app.js
@@ -17,6 +17,8 @@ const searchBook =  () => {
       fetch(url)
       .then(res => res.json())
       .then(data => displaySearchResult(data));
+
+      error.innerText = '';
     }
      
 }
@@ -34,26 +36,37 @@ const displaySearchResult =  books => {
     const totalResult = document.getElementById('total-result');
     totalResult.innerText = `Total Result Found: ${bookes.length}`;
 
-    // creating a div and dispaly the value --------------
-    for(const book in bookes){
-        // console.log(bookes[book]);
-
-        const div = document.createElement('div');
-        div.classList.add('col');
-        div.innerHTML = `
-            <div style="padding: 10px; background-color: #f6f6f6; border-radius: 10px" class="card h-100">
-                <img style="border-radius: 10px;" width="100%" src="https://covers.openlibrary.org/b/id/${bookes[book].cover_i}.jpg" />
-                <div class="card-body">
-                    
-                    <h5 style="margin-top: 10px" class="card-title">${bookes[book].title}</h5>
-                    <h6>Author Name: ${bookes[book].author_name}</h6>
-                    <p>${bookes[book].publish_date}
-                    </p>
-                    <p>Frist published year: ${bookes[book].first_publish_year}</p>
-                    <p>Publisher: ${bookes[book].publisher}</p>
-            </div>
-    </div>
-        `;
-        searchResult.appendChild(div);
+    if (bookes.length === 0){
+        const error2 = document.getElementById('error2');
+        error2.innerText = 'No result found';
     }
+    else{
+        // creating a div and dispaly the value --------------
+        for(const book in bookes){
+            // console.log(bookes[book]);
+    
+            const div = document.createElement('div');
+            div.classList.add('col');
+            div.innerHTML = `
+                <div style="padding: 10px; background-color: #f6f6f6; border-radius: 10px" class="card h-100">
+                    <img style="border-radius: 10px;" width="100%" src="https://covers.openlibrary.org/b/id/${bookes[book].cover_i}.jpg" />
+                    <div class="card-body">
+                        
+                        <h5 style="margin-top: 10px" class="card-title">${bookes[book].title}</h5>
+                        <h6>Author Name: ${bookes[book].author_name}</h6>
+                        <p>${bookes[book].publish_date}
+                        </p>
+                        <p>Frist published year: ${bookes[book].first_publish_year}</p>
+                        <p>Publisher: ${bookes[book].publisher}</p>
+                    </div>
+                </div>
+            `;
+            searchResult.appendChild(div);
+
+            error2.innerText ='';
+        }
+        
+    }
+    
+    
 }
